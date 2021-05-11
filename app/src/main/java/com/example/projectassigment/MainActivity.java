feae7947d86fa7e575df6263d74672b75cfb34e0
a20willa <larsson.william2000@gmail.com>
2021-05-11 10:15:08 +0200

    *Added menu with custom icon *Added a switch statment to activate menu item

--- app/src/main/java/com/example/projectassigment/MainActivity.java
+++ app/src/main/java/com/example/projectassigment/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.projectassigment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.settings_meny,menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Log.d("Text:","Success!");
+                return  true;
+
+            default:return super.onOptionsItemSelected(item);
+        }
     }
 }
